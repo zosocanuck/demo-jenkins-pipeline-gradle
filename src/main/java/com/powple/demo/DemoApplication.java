09b0a142a19c8fd1258ea3400bba73de09777414
Ivan Wallis <iwallis@gmail.com>
2022-10-12 12:45:40 -0700

    initial testing

--- src/main/java/com/powple/demo/DemoApplication.java
+++ src/main/java/com/powple/demo/DemoApplication.java
@@ -1,12 +1,8 @@
 package com.powple.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
